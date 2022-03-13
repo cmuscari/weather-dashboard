26a0387a1c90618d044bca60866b84f08bd8cfc4
Cortnie <cortniemuscari@gmail.com>
2022-03-13 01:23:47 -0600

    current weather information displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 // Global Variables
-var searchButtonEl = document.querySelector("#search-button")
+var searchButtonEl = document.querySelector("#search-button");
 var cityInputEl = document.querySelector("#city-name");
+var cityResultsEl = document.querySelector("#city-results");
+var currentDate = (moment().format("M/D/YYYY"));
+
 
 
 
 
 // Form submit handler function to be executed on form submission
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
-    
+
     // get value from input element
     var cityName = cityInputEl.value.trim();
 
@@ -16,6 +19,7 @@ var formSubmitHandler = function(event) {
     if (cityName) {
         getCityResults(cityName);
         cityInputEl.value = "";
+        return (cityName);
     }
     else {
         alert("Please enter a city name");
@@ -23,41 +27,34 @@ var formSubmitHandler = function(event) {
 };
 
 
+
+
+
+
+
 // Request the city results from the server
-var getCityResults = function(cityName) {
-    
+var getCityResults = function (cityName) {
     // format the Geocoding API url
     var geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=985c14e90aca4a530f8e1f738143f7b8";
-    // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
     // Make a request to the Geocoding API to get lattitude & longitude of city
-
-    // format the One Call API url (using the retrieved lattitude & longitude)
-    var lat = ;
-    var lon = ;
-    var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=" + part + "&appid=985c14e90aca4a530f8e1f738143f7b8";
-    // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-
-
-
-    
-    // make a request to the One Call API to get weather results
-    fetch(weatherUrl)
-        .then(function(response) {
+    fetch(geoUrl)
+        .then(function (response) {
             // request was successful
             if (response.ok) {
                 response.json().then(function(data) {
-                    // send the data into the displayRepos function
-                    displayRepos(data, user);
-                });
+                    // select the lat & long data & send them into the getWeatherResults function
+                    var lat = data[0].lat;
+                    var lon = data[0].lon;
+                    var city = cityName;
+                    getWeatherResults(lat, lon, city);
+                })
             }
             else {
-                alert("Error: GitHub User Not Found");
+                alert("Error: City Not Found");
             }
         })
         .catch(function(error) {
-            alert("Unable to connect to GitHub");
+            alert("Error: Unable to connect to One Call Weather");
         });
 }
 
@@ -65,5 +62,86 @@ var getCityResults = function(cityName) {
 
 
 
+// Request weather results from the server
+var getWeatherResults = function (lat, lon, city) {
+    // format the One Call API url (using the retrieved lattitude & longitude)
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=985c14e90aca4a530f8e1f738143f7b8";
+    // make a request to the One Call API to get weather results
+    fetch(weatherUrl)
+        .then(function(response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function(data) {
+                    
+                    // select the weather info to display in the city-results div container 
+                    var temp = data.current.temp;
+                    var wind = data.current.wind_speed;
+                    var humidity = data.current.humidity;
+                    var uvi = data.current.uvi;
+
+                    // create new weather info elements in the city-results div container
+                    var currentInfoEl = document.createElement("h2");
+                    currentInfoEl.classList = "flex-row justify-space-between font-weight-bold";
+                    currentInfoEl.textContent = city + " (" + currentDate + ")";
+                    cityResultsEl.appendChild(currentInfoEl);
+
+                    var currentTempEl = document.createElement("p");
+                    currentTempEl.classList = "flex-row justify-space-between font-weight-bold";
+                    currentTempEl.textContent = "Temp: " + temp + "°F";
+                    cityResultsEl.appendChild(currentTempEl);
+
+                    var currentWindEl = document.createElement("p");
+                    currentWindEl.classList = "flex-row justify-space-between font-weight-bold";
+                    currentWindEl.textContent = "Wind: " + wind + " MPH";
+                    cityResultsEl.appendChild(currentWindEl);
+
+                    var currentHumidityEl = document.createElement("p");
+                    currentHumidityEl.classList = "flex-row justify-space-between font-weight-bold";
+                    currentHumidityEl.textContent = "Humidity: " + humidity + "%";
+                    cityResultsEl.appendChild(currentHumidityEl);
+
+                    var currentUviEl = document.createElement("p");
+                    currentUviEl.classList = "flex-row justify-space-between font-weight-bold";
+                    currentUviEl.textContent = "UV Index: " + uvi;
+                    cityResultsEl.appendChild(currentUviEl);
+
+
+
+
+
+                })
+            }
+            else {
+                alert("Error: City Not Found");
+            }
+        })
+        .catch(function(error) {
+            alert("Error: Unable to connect to One Call Weather");
+        });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // When search button is clicked, run the formSubmitHandler function
-searchButtonEl.addEventListener("submit", formSubmitHandler);
+searchButtonEl.addEventListener("click", formSubmitHandler);
